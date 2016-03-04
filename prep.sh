4e9ab114a25a1cbf486b0d4a8f2c8d1691ed819f
Dmytro Gorbunov <Dmytro.Gorbunov@tomtom.com>
2016-03-04 18:18:16 +0100

    Add docker support

---
+++ prep.sh
@@ -0,0 +1,4 @@
+# docker run -it --rm  -p 8081:8081 --name nexus -v /tmp/nexus-data:/sonatype-work experiments/nexus
+export REPOSITORY_URL=http://localhost:8081
+export REPOSITORY_USER=admin
+export REPOSITORY_PASSWORD=admin123
